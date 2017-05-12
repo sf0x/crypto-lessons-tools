f93d0e3d318b0ab5a2073c3d97217b929ff5c804
sf0x <fox.troiza@gmail.com>
2017-05-12 14:59:13 +0200

    ec mul added, done but gui wip

--- ec/ec-point-add/ecpa.c
+++ ec/ec-point-add/ecpa.c
@@ -5,7 +5,150 @@
 #include <string.h>
 
 #define RABIN 15	// Miller-Rabin variable for testing if a number is prime
-#define OPTIONS "1) Point addition on curve\n2) Check if point is on curve\n0) Quit\n\nChoice: "
+#define OPTIONS "1) Point addition on curve\n2) Check if point is on curve\n3) Point Multiplication (naive)\n0) Quit\n\nChoice: "
+
+void point_add(mpz_t a, mpz_t b, mpz_t p, mpz_t Px, mpz_t Py, mpz_t Qx, mpz_t Qy, mpz_t Rx, mpz_t Ry, mpz_t infy){
+	mpz_t sRx,
+		  sRy,
+		  s;
+
+	mpz_init(sRx);
+	mpz_init(sRy);
+	mpz_init(s);
+
+	// check for infinity
+	mpz_sub(infy,p,Qy);
+	if((!mpz_cmp(Px,Qx))&&(!mpz_cmp(Py,infy))){
+		gmp_printf("\nQ is the inverse of P\nThe result is infinite!\n");
+		mpz_set_ui(Rx,0);
+		mpz_set_ui(Ry,0);
+	}else{
+		// Calculate s:
+
+		// Point doubling
+		if((!mpz_cmp(Px, Qx))&&(!mpz_cmp(Py, Qy))){
+			mpz_pow_ui(sRx, Px, 2);
+			mpz_mul_ui(sRx, sRx, 3);
+			mpz_add(sRx, sRx, a);
+			mpz_mod(sRx, sRx, p);
+			mpz_mul_ui(sRy, Py, 2);
+			mpz_mod(sRy, sRy, p);
+			mpz_invert(sRy, sRy, p);
+			// finally, merge s
+			mpz_mul(s, sRx, sRy);
+			mpz_mod(s, s, p);
+		}
+		// Point addition
+		else{
+			mpz_sub(sRy,Qy,Py);
+			if(mpz_cmp_ui(sRy,0)<0)
+				mpz_add(sRy,p,sRy);
+			mpz_sub(sRx,Qx,Px);
+			if(mpz_cmp_ui(sRx,0)<0)
+				mpz_add(sRx,p,sRx);
+			mpz_invert(sRx,sRx,p);
+			// merge s
+			mpz_mul(s,sRy,sRx);
+			mpz_mod(s,s,p);
+		}
+
+		// Calculate Rx:
+		mpz_pow_ui(Rx, s, 2);
+		mpz_sub(Rx, Rx, Px);
+		mpz_sub(Rx, Rx, Qx);
+		mpz_mod(Rx, Rx, p);
+
+		// Calculate Ry:
+		mpz_sub(Ry,Px,Rx);
+		mpz_mul(Ry,s,Ry);
+		mpz_sub(Ry,Ry,Py);
+		mpz_mod(Ry,Ry,p);
+
+		
+		// free variables only needed for R calculation
+		mpz_clear(sRx);
+		mpz_clear(sRy);
+		mpz_clear(s);
+	}
+}
+
+void point_mul(mpz_t a, mpz_t b, mpz_t p){
+	int iter=1, target=0;
+
+	mpz_t Px,
+	  	  Py,
+		  Qx,
+		  Qy,
+		  Rx,
+		  Ry,
+		  infy;
+
+	char *tmp, *garbage;	// temp and garbage vars for input handling
+	tmp=calloc(1,sizeof(long)+1);
+	long in;
+
+	mpz_init(Px);
+	mpz_init(Py);
+	mpz_init(Qx);
+	mpz_init(Qy);
+	mpz_init(Rx);
+	mpz_init(Ry);
+	mpz_init(infy);
+
+	gmp_printf("Please enter an start point\n");
+	gmp_printf("P(x): ");
+	fgets(tmp, sizeof(long)+1, stdin);
+	in=strtol(tmp, &garbage, sizeof(long)+2);
+	mpz_set_ui(Px,in);
+	memset(tmp, 0, sizeof(long)+1);	// zerofill tmp to avoid garbage
+
+	gmp_printf("P(y): ");
+	fgets(tmp, sizeof(long)+1, stdin);
+	in=strtol(tmp, &garbage, sizeof(long)+2);
+	mpz_set_ui(Py,in);
+	memset(tmp, 0, sizeof(long)+1);	// zerofill tmp to avoid garbage
+
+	gmp_printf("What point is this eg. 2P, 4P etc (from 2 to #E)...\n(x)P: ");
+	fgets(tmp, sizeof(long)+1, stdin);
+	iter=strtol(tmp, &garbage, sizeof(long)+2);
+	memset(tmp, 0, sizeof(long)+1);	// zerofill tmp to avoid garbage
+
+	gmp_printf("What point do you want to Multiplicate to?\n(x)P: ");
+	fgets(tmp, sizeof(long)+1, stdin);
+	target=strtol(tmp, &garbage, sizeof(long)+2);
+	memset(tmp, 0, sizeof(long)+1);	// zerofill tmp to avoid garbage
+
+	if((iter>target)||iter<0||target<0){
+		gmp_printf("Wrong input, ABORT!\n");
+		EXIT_FAILURE;
+	}
+	if(iter<2){
+		iter=2;
+		gmp_printf("Start to small, set to 2\n");
+	}
+
+
+	
+	mpz_set(Qx,Px);
+	mpz_set(Qy,Py);
+	for(int i=iter; i<target+1; i++){
+		//printf("iter: %d, target: %d\n", iter, target);
+		//printf("i: %d\n", i);
+		//puts("DEBUG2.x");
+		point_add(a,b,p,Px,Py,Qx,Qy,Rx,Ry,infy);
+		gmp_printf("%dP(%Zd,%Zd)\n",i,Rx,Ry);
+		mpz_set(Px,Rx);
+		mpz_set(Py,Ry);
+	}
+
+	mpz_clear(Px);
+	mpz_clear(Py);
+	mpz_clear(Qx);
+	mpz_clear(Qy);
+	mpz_clear(Rx);
+	mpz_clear(Ry);
+	mpz_clear(infy);
+}
 
 // Check if an point P is on the elliptic curve
 void proof(mpz_t a, mpz_t b, mpz_t p){
@@ -58,15 +201,12 @@ void proof(mpz_t a, mpz_t b, mpz_t p){
 	mpz_clear(xtwo);
 }
 
-void point_add(mpz_t a, mpz_t b, mpz_t p){
+void point_add_input(mpz_t a, mpz_t b, mpz_t p){
 	// point parameters
 	mpz_t Px,
 		  Py,
 		  Qx,
 		  Qy,
-		  sRx,
-		  sRy,
-		  s,
 		  Rx,
 		  Ry,
 		  infy;
@@ -79,9 +219,6 @@ void point_add(mpz_t a, mpz_t b, mpz_t p){
 	mpz_init(Py);
 	mpz_init(Qx);
 	mpz_init(Qy);
-	mpz_init(sRx);
-	mpz_init(sRy);
-	mpz_init(s);
 	mpz_init(Rx);
 	mpz_init(Ry);
 	mpz_init(infy);
@@ -112,66 +249,16 @@ void point_add(mpz_t a, mpz_t b, mpz_t p){
 	memset(tmp, 0, sizeof(long)+1);	// zerofill tmp to avoid garbage
 
 
-	// check for infinity
-	mpz_sub(infy,p,Qy);
-	if((!mpz_cmp(Px,Qx))&&(!mpz_cmp(Py,infy))){
-		gmp_printf("\nQ is the inverse of P\nThe result is infinite!\n");
-	}else{
-		// Calculate s:
+	
+	point_add(a, b, p, Px,Py,Qx,Qy,Rx,Ry,infy);
 
-		// Point doubling
-		if((!mpz_cmp(Px, Qx))&&(!mpz_cmp(Py, Qy))){
-			mpz_pow_ui(sRx, Px, 2);
-			mpz_mul_ui(sRx, sRx, 3);
-			mpz_add(sRx, sRx, a);
-			mpz_mod(sRx, sRx, p);
-			mpz_mul_ui(sRy, Py, 2);
-			mpz_mod(sRy, sRy, p);
-			mpz_invert(sRy, sRy, p);
-			// finally, merge s
-			mpz_mul(s, sRx, sRy);
-			mpz_mod(s, s, p);
-		}
-		// Point addition
-		else{
-			mpz_sub(sRy,Qy,Py);
-			if(mpz_cmp_ui(sRy,0)<0)
-				mpz_add(sRy,p,sRy);
-			mpz_sub(sRx,Qx,Px);
-			if(mpz_cmp_ui(sRx,0)<0)
-				mpz_add(sRx,p,sRx);
-			mpz_invert(sRx,sRx,p);
-			// merge s
-			mpz_mul(s,sRy,sRx);
-			mpz_mod(s,s,p);
-		}
-
-		// Calculate Rx:
-		mpz_pow_ui(Rx, s, 2);
-		mpz_sub(Rx, Rx, Px);
-		mpz_sub(Rx, Rx, Qx);
-		mpz_mod(Rx, Rx, p);
-
-		// Calculate Ry:
-		mpz_sub(Ry,Px,Rx);
-		mpz_mul(Ry,s,Ry);
-		mpz_sub(Ry,Ry,Py);
-		mpz_mod(Ry,Ry,p);
-
-		// resulting point
-		gmp_printf("\nR(%Zd,%Zd)\n",Rx,Ry);
-	}
-
-	// proof
-
+	// resulting point
+	gmp_printf("\nR(%Zd,%Zd)\n",Rx,Ry);
 
 	mpz_clear(Px);
 	mpz_clear(Py);
 	mpz_clear(Qx);
 	mpz_clear(Qy);
-	mpz_clear(sRx);
-	mpz_clear(sRy);
-	mpz_clear(s);
 	mpz_clear(Rx);
 	mpz_clear(Ry);
 	mpz_clear(infy);
@@ -265,9 +352,11 @@ int main(int argc, char *argv[]){
 		if(!opt)
 			break;
 		else if(opt==1)
-			point_add(a,b,p);
+			point_add_input(a,b,p);
 		else if(opt==2)
 			proof(a,b,p);
+		else if(opt==3)
+			point_mul(a,b,p);
 		else
 			gmp_printf("Not an valid option!\n");
 		memset(tmp_opt, 0, sizeof(short));
